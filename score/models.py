a0a8586e81e22ee2f4130cfd32a73e3fdef73db8
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-11-16 11:31:34 +0100

    Add Score and CurrentScore models to score app

--- score/models.py
+++ score/models.py
@@ -1,3 +1,45 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db.models import (Model, ForeignKey, CharField, IntegerField,
+                              DateTimeField, OneToOneField)
+from django.forms import ModelForm
+
+from django.contrib.auth.models import User
+
+
+class Score(Model):
+    GAMES = (
+        ('c', 'checkpoints'),
+    )
+
+    user = ForeignKey(User)
+    game = CharField(max_length=1, choices=GAMES)
+    value = IntegerField()
+    date = DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return '{} scored {} ({})'.format(self.user, self.value, self.game)
+
+
+class ScoreForm(ModelForm):
+    class Meta:
+        model = Score
+        fields = ('user', 'game', 'value',)
+
+
+class CurrentScore(Model):
+    user = OneToOneField(User)
+    value = IntegerField()
+
+    def add(self, value):
+        self.value += value
+        self.save()
+
+    def __unicode__(self):
+        return '{} has {} points'.format(self.user, self.value)
+
+
+class CurrentScoreForm(ModelForm):
+    class Meta:
+        model = CurrentScore
+        fields = ('user', 'value',)
