0bbc0a78c8796532473ecf4a7523b00fe5d4b4ef
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-11-25 10:31:45 +0100

    Add Profile model storing informations about users through a OneToOne relation

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from users.models import Profile, ProfileForm
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    form = ProfileForm
+    list_display = ('user', 'rfid_tag', 'favorite_song',)
+    search_fields = ('user__username', 'rfid_tag', 'favorite_song',)
+
+admin.site.register(Profile, ProfileAdmin)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,21 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db.models import Model, OneToOneField, CharField
+from django.forms import ModelForm
+
+from django.contrib.auth.models import User
+
+
+class Profile(Model):
+    user = OneToOneField(User)
+    rfid_tag = CharField(max_length=64)
+    favorite_song = CharField(max_length=64)
+
+    def __unicode__(self):
+        return '{}\'s profile'.format(self.user)
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('user', 'rfid_tag', 'favorite_song',)
