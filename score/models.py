de2751afa088947bcf64aac892616c553578f439
Jean-Baptiste Braun <jbaptiste.braun@gmail.com>
2015-12-03 12:51:22 +0100

    Add score user comments

--- score/models.py
+++ score/models.py
@@ -9,6 +9,18 @@ from django.contrib.auth.models import User
 
 
 class Score(Model):
+    """
+    A Score entry is a discrete differential value at a given date for a user
+    refering to a game.
+
+    Instead of recording a single score value for each user, it allows
+    flexibility to display score tables that can be filtered for a particular
+    game and/or between two dates.
+
+    For example, we can imagine a monthly event where people could see
+    everyone else's scores before setting the time period for the next month.
+    """
+
     GAMES = (
         ('p', 'checkpoints'),
         ('c', 'coffeecups'),
@@ -21,6 +33,12 @@ class Score(Model):
     date = DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
+        """
+        A score entry also updates the user's current score. Current scores
+        is a single value per user recording all score deltas since the
+        beggining of the game.
+        """
+
         if self.pk:
             old_score = Score.objects.get(pk=self.pk)
             try:
@@ -55,6 +73,14 @@ class ScoreForm(ModelForm):
 
 
 class CurrentScore(Model):
+    """
+    Current scores are a single value per user recording all score deltas
+    since the beggining of the game.
+
+    It can be used to simulates levels for people to reach, and give them
+    rewards in function of their value since the beggining for instance.
+    """
+
     user = OneToOneField(User)
     value = IntegerField()
 
--- score/views.py
+++ score/views.py
@@ -16,6 +16,20 @@ from score.filters import ScoreFilter, CurrentScoreFilter
 @authentication_classes((TokenAuthentication, SessionAuthentication,))
 @permission_classes((IsAuthenticated,))
 class ScoreView(ReadOnlyModelViewSet):
+    """
+    A Score entry is a discrete differential value at a given date for a user
+    refering to a game.
+
+    A score entry also updates the user's current score.
+
+    Instead of recording a single score value for each user, it allows
+    flexibility to display score tables that can be filtered for a particular
+    game and/or between two dates.
+
+    For example, we can imagine a monthly event where people could see
+    everyone else's scores before setting the time period for the next month.
+    """
+
     queryset = Score.objects.all()
     serializer_class = ScoreSerializer
     filter_backends = (DjangoFilterBackend, SearchFilter,)
@@ -26,6 +40,14 @@ class ScoreView(ReadOnlyModelViewSet):
 @authentication_classes((TokenAuthentication, SessionAuthentication,))
 @permission_classes((IsAuthenticated,))
 class CurrentScoreView(ReadOnlyModelViewSet):
+    """
+    Current scores are a single value per user recording all score deltas
+    since the beggining of the game.
+
+    It can be used to simulates levels for people to reach, and give them
+    rewards in function of their value since the beggining for instance.
+    """
+
     queryset = CurrentScore.objects.all()
     serializer_class = CurrentScoreSerializer
     filter_backends = (DjangoFilterBackend,)
